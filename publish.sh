160bb75b8aca372764885cc2c0feadf5e95a20d1
McKade Umbenhower <mckade22@gmail.com>
2021-05-23 10:16:16 -0400

    Updating hugo build to clean

--- publish.sh
+++ publish.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
+# Retrieve args
+if [ $# -ne 1 ]
+then
+	echo "Usage: ./publish \"commit message\""
+	exit 1
+fi
+
 MSG=$1
 
+# Build the site
+mv docs/CNAME .
+rm -rf docs
+
 hugo
+if [ $? -ne 0 ]
+then
+	echo "Hugo failed to build. Cleaning up."
+	mkdir docs
+	mv CNAME docs/
+	exit $?
+fi
+
+mv CNAME docs/
+
+# Push to github and publish the site
 git add .
 git commit -m "$MSG"
 git push
